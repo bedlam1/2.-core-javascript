38f084b04746f9c4064f9ecd3eb6144a7f2e3718
bedlam1 <lhj03265@gmail.com>
2024-01-18 15:12:18 +0900

    core_javascript

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars" : "off" // 변수선언에러 무시
+    }
+}
---
+++ client/main.js
@@ -0,0 +1,16 @@
+/* eslint no-undef: off */  
+hello()
+
+const a = 10;
+
+console.log("자바스크립트!");
+
+
+
+
+
+
+
+
+
+
