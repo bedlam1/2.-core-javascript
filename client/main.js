68e5d59e59fedac9209c6d5a8f5041f3415a9846
bedlam3 <lhj03265@gmail.com>
2024-01-19 13:03:32 +0900

    20240119_1 modify

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
-/* eslint no-undef: off */  
-hello()
 
-const a = 10;
+/* window.alert, window.confirm ...  */
+// alert('마우스 우클릭은 사용할 수 없습니다.');
+// confirm('정말 이 페이지를 벗어나시겠습니까?');
+// prompt('너 이름이 뭐니?');
 
-console.log("자바스크립트!");
+// console.log();
+
+
 
 
 
